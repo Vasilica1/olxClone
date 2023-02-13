67d7bfc815a742df52d5961b799e53709455863f
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2023-02-13 17:37:58 +0200

    change the auth component and add the add component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,27 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLogIn:boolean = true;
+  textChange: string = "Intra in cont";
+
+  switchToCreateAccount() {
+    this.isLogIn = false;
+    this.textChange = "Creeaza un cont";
+  }
+
+  swithTologIn() {
+    this.isLogIn = true;
+    this.textChange = "Intra in cont";
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
+}
